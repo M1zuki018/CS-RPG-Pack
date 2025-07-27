638897999f0b7ea5644c01067999f3b6381fb039
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 11:13:47 +0900

    fix:テキスト表示を行うオーダーにかける時間の計算方法を修正

--- Assets/iCON/Scripts/System/Story/Executor/OrderHandler/DescriptiveOrderHandler.cs
+++ Assets/iCON/Scripts/System/Story/Executor/OrderHandler/DescriptiveOrderHandler.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using iCON.Constants;
 using iCON.Enums;
 using iCON.UI;
 
@@ -14,8 +15,15 @@ namespace iCON.System
         
         public override Tween HandleOrder(OrderData data, StoryView view)
         {
+            // テキストの上書きスピードが設定されていたらそのスピードを使用
+            // 設定されていない場合は定数を使用する
+            var multiply = data.OverrideTextSpeed != 0 ? data.OverrideTextSpeed : KStoryPresentation.DIALOG_TEXT_SPEED;
+            
+            // テキスト更新にかける時間を計算
+            var duration = data.DialogText.Length * multiply;
+            
             // 名前なしのダイアログを表示する
-            return view.SetDescription(data.DialogText, data.Duration);
+            return view.SetDescription(data.DialogText, duration);
         }
     }
 }
