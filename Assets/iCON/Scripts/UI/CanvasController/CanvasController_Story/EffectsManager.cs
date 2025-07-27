3f8ed9553ed7e7bbc91ef2554f0e37cc6d89e143
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 15:55:42 +0900

    add:エフェクトマネージャー仮実装

---
+++ Assets/iCON/Scripts/UI/CanvasController/CanvasController_Story/EffectsManager.cs
@@ -0,0 +1,29 @@
+using iCON.System;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour
+{
+    // TODO: 仮実装
+    public static EffectsManager Instance;
+    [SerializeField] private DizzinessEffectController _dizzinessEffectController;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    /// <summary>
+    /// めまいエフェクトを再生/停止する
+    /// </summary>
+    public void DizzinessEffect(bool isActive)
+    {
+        if (isActive)
+        {
+            _dizzinessEffectController.TriggerDizzinessEffect();
+        }
+        else
+        {
+            _dizzinessEffectController.StopAndResetEffect();
+        }
+    }
+}
