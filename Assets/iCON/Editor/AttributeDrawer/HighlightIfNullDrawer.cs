fc8df45a287c4d007aa201ec3042ad5958d9e7d1
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 17:04:24 +0900

    fix:名前空間を追加

--- Assets/iCON/Editor/AttributeDrawer/HighlightIfNullDrawer.cs
+++ Assets/iCON/Editor/AttributeDrawer/HighlightIfNullDrawer.cs
@@ -2,25 +2,28 @@ using CryStar.Attribute;
 using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// オブジェクトが未割当の場合プロパティの背景色を変更する
-/// </summary>
-[CustomPropertyDrawer(typeof(HighlightIfNullAttribute))]
-public class HighlightIfNullDrawer : PropertyDrawer
+namespace CryStar.Editor
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    /// <summary>
+    /// オブジェクトが未割当の場合プロパティの背景色を変更する
+    /// </summary>
+    [CustomPropertyDrawer(typeof(HighlightIfNullAttribute))]
+    public class HighlightIfNullDrawer : PropertyDrawer
     {
-        // ObjectReference型で、値がnullの場合に背景色を変更
-        bool isNull = property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null;
-
-        Color defaultColor = GUI.backgroundColor; // デフォルトの背景色を保存
-
-        if (isNull)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            GUI.backgroundColor = Color.red; // 未割り当ての場合、背景色を赤に変更
-        }
+            // ObjectReference型で、値がnullの場合に背景色を変更
+            bool isNull = property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null;
+
+            Color defaultColor = GUI.backgroundColor; // デフォルトの背景色を保存
+
+            if (isNull)
+            {
+                GUI.backgroundColor = Color.red; // 未割り当ての場合、背景色を赤に変更
+            }
         
-        EditorGUI.PropertyField(position, property, label); // プロパティを描画
-        GUI.backgroundColor = defaultColor; // 背景色を元に戻す
+            EditorGUI.PropertyField(position, property, label); // プロパティを描画
+            GUI.backgroundColor = defaultColor; // 背景色を元に戻す
+        }
     }
 }
