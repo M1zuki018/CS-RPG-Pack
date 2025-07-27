4d3d90da5fdeba305b5da9cbfaecf8269d6c0420
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 16:41:15 +0900

    fix:名前空間を追加

--- Assets/iCON/Attribute/CommentAttribute.cs
+++ Assets/iCON/Attribute/CommentAttribute.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using System;
 
-/// <summary>
-/// 変数名をコメントに書き換えるカスタム属性
-/// NOTE: この属性はMonoBehaviourやScriptableObjectの直接のフィールドにのみ適用されます
-/// [Serializable]クラス内のフィールドには適用されません
-/// 使用例: [SerializeField, Comment("プレイヤーの体力")] private int _health;
-/// </summary>
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
-public class CommentAttribute : PropertyAttribute
+namespace CryStar.Attribute
 {
-    public string Text { get; }
-
-    public CommentAttribute(string text)
+    /// <summary>
+    /// 変数名をコメントに書き換えるカスタム属性
+    /// NOTE: この属性はMonoBehaviourやScriptableObjectの直接のフィールドにのみ適用されます
+    /// [Serializable]クラス内のフィールドには適用されません
+    /// 使用例: [SerializeField, Comment("プレイヤーの体力")] private int _health;
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public class CommentAttribute : PropertyAttribute
     {
-        Text = text;
+        public string Text { get; }
+
+        public CommentAttribute(string text)
+        {
+            Text = text;
+        }
     }
 }
