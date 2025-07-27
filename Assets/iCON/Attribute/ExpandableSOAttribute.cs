f703e19c032cbde8cca14586e809e6ce3d2409b0
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 16:41:24 +0900

    fix:名前空間を追加

--- Assets/iCON/Attribute/ExpandableSOAttribute.cs
+++ Assets/iCON/Attribute/ExpandableSOAttribute.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 /// ScriptableObjectをInspector上で展開して編集できるようにする属性
 /// </summary>
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-public class ExpandableSOAttribute : PropertyAttribute { }
+public class ExpandableSOAttribute : PropertyAttribute
+{
+    /// <summary>
+    /// 展開表示するかどうかの初期状態
+    /// </summary>
+    public bool DefaultExpanded { get; }
+
+    /// <summary>
+    /// 背景スタイルを使用するかどうか
+    /// </summary>
+    public bool UseBackground { get; }
+
+    public ExpandableSOAttribute(bool defaultExpanded = true, bool useBackground = true)
+    {
+        DefaultExpanded = defaultExpanded;
+        UseBackground = useBackground;
+    }
+}
