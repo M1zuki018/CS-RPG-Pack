0ab16ba0c45077d2598462cbaf5fcba05248eff7
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 17:03:08 +0900

    fix:名前空間を追加

--- Assets/iCON/Attribute/HighlightIfNullAttribute.cs
+++ Assets/iCON/Attribute/HighlightIfNullAttribute.cs
@@ -1,8 +1,11 @@
 using System;
 using UnityEngine;
 
-/// <summary>
-/// 参照が割り当てられていないフィールドをハイライトする属性
-/// </summary>
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-public class HighlightIfNullAttribute : PropertyAttribute { }
+namespace CryStar.Attribute
+{
+    /// <summary>
+    /// 参照が割り当てられていないフィールドをハイライトする属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class HighlightIfNullAttribute : PropertyAttribute { }
+}
