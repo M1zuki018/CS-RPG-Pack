3c209c68870ad694adca32b96a2c3d451ca4c4d9
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-27 10:15:42 +0900

    fix:EffectのFactoryを利用する形に修正

--- Assets/iCON/Scripts/System/Story/Executor/OrderHandler/EffectOrderHandler.cs
+++ Assets/iCON/Scripts/System/Story/Executor/OrderHandler/EffectOrderHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using iCON.Enums;
 using iCON.UI;
@@ -10,12 +11,24 @@ namespace iCON.System
     [OrderHandler(OrderType.Effect)]
     public class EffectOrderHandler : OrderHandlerBase
     {
+        /// <summary>
+        /// 各エフェクトの列挙型と処理を行うPerformerのインスタンスのkvp
+        /// </summary>
+        private Dictionary<EffectOrderType, EffectOrderPerformerBase> _performers;
+        
         public override OrderType SupportedOrderType => OrderType.Effect;
+
+        /// <summary>
+        /// Setup
+        /// </summary>
+        public void SetupPerformerCache(StoryView view)
+        {
+            _performers = EffectOrderPerformerFactory.CreateAllHandlers(view);
+        }
         
         public override Tween HandleOrder(OrderData data, StoryView view)
         {
-            // TODO
-            return null;
+            return _performers[(EffectOrderType)data.SpeakerId].HandlePerformance(data, view);
         }
     }
 }
