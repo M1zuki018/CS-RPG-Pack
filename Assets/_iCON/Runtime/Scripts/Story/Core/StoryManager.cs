4fc74da5e124a5368f8dd29f88f2090e5664a099
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-29 00:55:51 +0900

    feat:ストーリーマネージャー実装

--- Assets/_iCON/Runtime/Scripts/Story/Core/StoryManager.cs
+++ Assets/_iCON/Runtime/Scripts/Story/Core/StoryManager.cs
@@ -25,49 +25,119 @@ namespace CryStar.Story.Core
         private StoryDataLoader _dataLoader = new StoryDataLoader();
         
         /// <summary>
-        /// 読み込んだオーダーデータのキャッシュ
+        /// ストーリーIDと読み込んだオーダーデータのキャッシュのkvp
         /// </summary>
-        private List<OrderData> _orders;
+        private Dictionary<int, List<OrderData>> _ordersCache = new Dictionary<int, List<OrderData>>();
+        
+        /// <summary>
+        /// 初期化済みか
+        /// </summary>
+        private bool _isInitialized = false;
+
+        #region Life cycle
+
+        private void OnDestroy()
+        {
+            ClearAllCache();
+        }
+
+        #endregion
+        
+        /// <summary>
+        /// ストーリーを再生する
+        /// </summary>
+        public async UniTask PlayStoryAsync(int sceneId, Action endAction)
+        {
+            // シーンIDを元にシーンマスタを取得
+            if (!TryGetMasterData(sceneId, out var storySceneData))
+            {
+                return;
+            }
+            
+            // 指定されたオーダーを取得
+            await LoadSceneDataAsync(sceneId);
+            
+            // ストーリー再生
+            _player.PlayStory(storySceneData, _ordersCache[sceneId], endAction);
+        }
         
         /// <summary>
         /// 指定範囲のデータを読み込んでSceneDataを作成する
+        /// NOTE: 事前にロードしておきたい場合などはこのメソッドだけ呼び出せばOK
         /// </summary>
-        public async UniTask LoadSceneDataAsync(string spreadsheetName, string range)
+        public async UniTask LoadSceneDataAsync(int sceneId)
         {
-            _orders = await _dataLoader.LoadSceneDataAsync(spreadsheetName, range);
-        }
-
-        public async UniTask PlayStory(int sceneId, Action endAction)
-        {
-            var storySceneData = MasterStoryScene.GetSceneById(sceneId);
-            if (storySceneData == null)
+            if (_ordersCache.ContainsKey(sceneId))
             {
-                LogUtility.Error($"ストーリーが見つかりませんでした: {sceneId}", LogCategory.System);
+                // 既にキャッシュが生成されていればスキップ
+                return;
             }
             
-            if (_orders == null)
+            if (!_isInitialized)
             {
-                await InitializeAsync(KStoryPresentation.SPREAD_SHEET_NAME, RangeBuilder(KStoryPresentation.HEADER_RANGE));
-                await LoadSceneDataAsync(KStoryPresentation.SPREAD_SHEET_NAME, RangeBuilder(storySceneData.Range));
+                // ヘッダーの初期化を行っていない場合は先にヘッダーの初期化を行う
+                await InitializeAsync();
             }
             
-            _player.Play(_orders);
-            await _player.PlayStory(storySceneData, endAction);
+            if (TryGetMasterData(sceneId, out var sceneData))
+            { 
+                // オーダーリストを取得した後、辞書に登録
+                var orders = await _dataLoader.LoadSceneDataAsync(KStoryPresentation.SPREAD_SHEET_NAME, BuildSheetRange(sceneData.Range));
+                _ordersCache[sceneId] = orders;
+            }
         }
 
-        private string RangeBuilder(string range)
+        /// <summary>
+        /// 指定したシーンのキャッシュをクリアする
+        /// </summary>
+        public void ClearCache(int sceneId)
         {
-            return KStoryPresentation.SPREAD_SHEET_NAME + "!" + range;
+            _ordersCache[sceneId] = null;
         }
+        
+        /// <summary>
+        /// キャッシュをクリアする
+        /// </summary>
+        public void ClearAllCache()
+        {
+            _ordersCache.Clear();
+        }
+        
+        #region Private methods
         
         /// <summary>
         /// 初期化処理
         /// </summary>
-        private async UniTask InitializeAsync(string spreadsheetName, string headerRange)
+        private async UniTask InitializeAsync()
         {
             // ヘッダー行を読み込んでインデックスマップを作成
             // ヘッダー行が全て共通している場合は呼びなおしは不要
-            await _dataLoader.InitializeAsync(spreadsheetName, headerRange);
+            await _dataLoader.InitializeAsync(KStoryPresentation.SPREAD_SHEET_NAME, BuildSheetRange(KStoryPresentation.HEADER_RANGE));
+            _isInitialized = true;
         }
+        
+        /// <summary>
+        /// マスタからStorySceneDataを取得してnullチェックを行う
+        /// </summary>
+        private bool TryGetMasterData(int sceneId, out StorySceneData sceneData)
+        {
+            sceneData = MasterStoryScene.GetSceneById(sceneId);
+            if (sceneData == null)
+            {
+                LogUtility.Error($"ストーリーが見つかりませんでした: {sceneId}", LogCategory.System);
+                return false;
+            }
+            return true;
+        }
+        
+        /// <summary>
+        /// スプレッドシートの範囲指定文字列を構築する
+        /// </summary>
+        private string BuildSheetRange(string range)
+        {
+            return $"{KStoryPresentation.SPREAD_SHEET_NAME}!{range}";
+        }
+        
+        #endregion
     }
 }
