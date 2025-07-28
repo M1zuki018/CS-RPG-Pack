79df5a09e12c46c7ba924383592a93d8e391a96d
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-28 20:54:37 +0900

    rename:StoryExecuteDataSO -> SceneDataSO

--- Assets/iCON/Scripts/System/Story/Data/SceneDataSO.cs
+++ Assets/iCON/Scripts/System/Story/Data/SceneDataSO.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using CryStar.Attribute;
 using UnityEngine;
 
@@ -6,18 +7,21 @@ namespace iCON.System
     /// <summary>
     /// ストーリーを実行するために必要なデータを設定するためのスクリプタブルオブジェクト
     /// </summary>
-    [CreateAssetMenu(fileName = "StoryExecuteDataSO", menuName = "Scriptable Objects/StoryExecuteDataSO")]
-    public class StoryExecuteDataSO : ScriptableObject
+    [CreateAssetMenu(fileName = "SceneDataSO", menuName = "Scriptable Objects/SceneDataSO")]
+    public class SceneDataSO : ScriptableObject
     {
         [Header("再生するストーリーの設定")]
         [SerializeField]
         private StoryLine _storyLine;
-
+        
         [SerializeField, Comment("キャラクターの立ち絵の拡大率")]
         private float _characterScale = 1;
 
-        [Comment("キャラクターの立ち絵の位置補正量")]
-        public Vector3 _characterPositionOffset = Vector3.zero;
+        [SerializeField, Comment("キャラクターの立ち絵の位置補正量")]
+        private Vector3 _characterPositionOffset = Vector3.zero;
+        
+        [SerializeField, Comment("前提ストーリー")]
+        private StorySaveData[] _premiseStoryArray;
         
         /// <summary>
         /// 再生するストーリーの設定
@@ -33,5 +37,13 @@ namespace iCON.System
         /// キャラクターの立ち絵の位置補正量
         /// </summary>
         public Vector3 CharacterPositionOffset => _characterPositionOffset;
+
+        /// <summary>
+        /// 前提ストーリーを全てクリアしているか
+        /// </summary>
+        public bool IsPremiseAllStoryClear()
+        {
+            return _premiseStoryArray.All(StoryUserData.IsPremiseStoryClear);
+        }
     }
 }
