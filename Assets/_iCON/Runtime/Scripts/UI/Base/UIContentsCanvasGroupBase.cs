ddb6db6cb6fcd783f03992c47872e7ba22d05b09
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-31 01:00:34 +0900

    feat:CanvasGroupを使うUIContentsのベースクラスを実装

---
+++ Assets/_iCON/Runtime/Scripts/UI/Base/UIContentsCanvasGroupBase.cs
@@ -0,0 +1,64 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace CryStar.UI
+{
+    /// <summary>
+    /// CanvasGroupを利用するUIContentsクラスのベースクラス
+    /// </summary>
+    [RequireComponent(typeof(CanvasGroup))]
+    public abstract class UIContentsCanvasGroupBase : UIContentsBase, IFadeable
+    {
+        /// <summary>
+        /// CanvasGroup
+        /// </summary>
+        protected CanvasGroup _canvasGroup;
+        
+        /// <summary>
+        /// 現在表示されているか
+        /// </summary>
+        public bool IsVisible => _canvasGroup != null && _canvasGroup.alpha > 0;
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        public override void Initialize()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        /// <summary>
+        /// 表示状態を設定する
+        /// </summary>
+        public virtual void SetVisibility(bool isVisible)
+        {
+            _canvasGroup.alpha = isVisible ? 1 : 0;
+            _canvasGroup.interactable = isVisible;
+            _canvasGroup.blocksRaycasts = isVisible;
+        }
+
+        /// <summary>
+        /// フェードイン
+        /// </summary>
+        public virtual Tween FadeIn(float duration)
+        {
+            return _canvasGroup.DOFade(1, duration);
+        }
+
+        /// <summary>
+        /// フェードアウト
+        /// </summary>
+        public virtual Tween FadeOut(float duration)
+        {
+            return _canvasGroup.DOFade(0, duration);
+        }
+
+        /// <summary>
+        /// 指定したアルファ値までフェード
+        /// </summary>
+        public virtual Tween FadeToAlpha(float targetAlpha, float duration)
+        {
+            return _canvasGroup.DOFade(targetAlpha, duration);
+        }
+    }
+}
