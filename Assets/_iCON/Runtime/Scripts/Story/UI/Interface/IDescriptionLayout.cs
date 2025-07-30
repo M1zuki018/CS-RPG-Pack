cb5ce810df0b6acae7f9d1badf5ef37536b2f227
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-30 18:47:06 +0900

    feat:地の文用のインターフェース

---
+++ Assets/_iCON/Runtime/Scripts/Story/UI/Interface/IDescriptionLayout.cs
@@ -0,0 +1,10 @@
+namespace CryStar.UI
+{
+    /// <summary>
+    /// 地の文ダイアログレイアウト
+    /// </summary>
+    public interface IDescriptionLayout : ITextDisplayable
+    {
+        // ITextDisplayableから継承されるメソッドで十分なので追加はなし
+    }
+}
