170388e8898bd04810c170507542930c10d94d2b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-30 18:47:44 +0900

    feat:スチル制御のインターフェース

---
+++ Assets/_iCON/Runtime/Scripts/Story/UI/Interface/ISteelController.cs
@@ -0,0 +1,14 @@
+using CryStar.UI;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+
+namespace CryStar.Story.UI
+{
+    /// <summary>
+    /// スチル制御機能
+    /// </summary>
+    public interface ISteelController : IImageControllable, IFadeable
+    {
+        // 追加はなし
+    }
+}
