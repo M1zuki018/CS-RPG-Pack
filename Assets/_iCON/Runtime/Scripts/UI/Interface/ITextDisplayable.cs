49cfadccc2b61e5e72a60d310d25fa81ce26d3b4
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-31 01:26:40 +0900

    fix:コメント追加

--- Assets/_iCON/Runtime/Scripts/UI/Interface/ITextDisplayable.cs
+++ Assets/_iCON/Runtime/Scripts/UI/Interface/ITextDisplayable.cs
@@ -3,11 +3,18 @@ using DG.Tweening;
 namespace CryStar.UI
 {
     /// <summary>
-    /// Tweenを使ったテキスト表示機能を持つUIContents
+    /// Tweenを使ったテキスト表示機能を持つUIContentsが継承すべきインターフェース
     /// </summary>
     public interface ITextDisplayable
     {
+        /// <summary>
+        /// テキストをセットする
+        /// </summary>
         Tween SetText(string text, float duration = 0f);
+        
+        /// <summary>
+        /// テキストをクリアする
+        /// </summary>
         void ClearText();
     }
 }
