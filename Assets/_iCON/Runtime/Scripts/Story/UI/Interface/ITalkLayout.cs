7f4e64a3ba00397c89c9d59c08b977f8e01ea3bf
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-31 01:26:22 +0900

    fix:コメント追加

--- Assets/_iCON/Runtime/Scripts/Story/UI/Interface/ITalkLayout.cs
+++ Assets/_iCON/Runtime/Scripts/Story/UI/Interface/ITalkLayout.cs
@@ -3,12 +3,23 @@ using DG.Tweening;
 namespace CryStar.UI
 {
     /// <summary>
-    /// 名前付きダイアログレイアウト
+    /// 名前付きダイアログのUIContentsが継承すべきインターフェース
     /// </summary>
     public interface ITalkLayout : ITextDisplayable
     {
+        /// <summary>
+        /// 表示テキストを変更する
+        /// </summary>
         Tween SetTalk(string name, string dialog, float duration = 0f);
+        
+        /// <summary>
+        /// 名前のみを設定する
+        /// </summary>
         void SetName(string name);
+        
+        /// <summary>
+        /// 会話文のみを設定する
+        /// </summary>
         Tween SetDialog(string dialog, float duration = 0f);
     }
 }
