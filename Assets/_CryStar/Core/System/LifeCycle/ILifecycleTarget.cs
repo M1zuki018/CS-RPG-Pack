9711af7e153cb6cc19d3d75e6cd2ab722408fc2c
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-31 11:31:40 +0900

    feat:ライフサイクルのベースクラスに継承させるインターフェースを追加

---
+++ Assets/_CryStar/Core/System/LifeCycle/ILifecycleTarget.cs
@@ -0,0 +1,30 @@
+using Cysharp.Threading.Tasks;
+
+namespace CryStar.Core
+{
+    /// <summary>
+    /// ライフサイクル管理対象のインターフェース
+    /// </summary>
+    public interface ILifecycleTarget
+    {
+        /// <summary>
+        /// 他クラスに干渉しない初期化処理
+        /// </summary>
+        UniTask OnAwake();
+        
+        /// <summary>
+        /// UI表示の初期化処理
+        /// </summary>
+        UniTask OnUIInitialize();
+        
+        /// <summary>
+        /// event Actionの登録など他のクラスと干渉する処理
+        /// </summary>
+        UniTask OnBind();
+        
+        /// <summary>
+        /// ゲーム開始前最後に実行される処理
+        /// </summary>
+        UniTask OnStart();
+    }
+}
